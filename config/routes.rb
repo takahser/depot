269cd6c11e41942c6ee7aadf12b6efeb1877fb84
seraya <takahser@students.zhaw.ch>
2014-06-08 22:47:10 +0200

    made a lot of changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
 
@@ -6,6 +10,7 @@ Depot::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'store#index', as: 'store'
   # root 'welcome#index'
 
   # Example of regular route:
